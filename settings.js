cbdc9f2c3bda238370c8c14c6cfaed7e214eaf51
safatima209-star <safatima209@gmail.com>
2026-05-28 17:24:35 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
 const settings = {
-  packname: 'Knight Bot',
-  author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
+  packname: 'STC',
+  author: 'Bot‎',
+  botName: "STC Bot",
+  botOwner: 'STC Bot', // Your name
   ownerNumber: '212617503205', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
