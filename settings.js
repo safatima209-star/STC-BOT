cc72f8f59358bba832bed8d5c0dbba123a06808a
safatima209-star <safatima209@gmail.com>
2026-05-30 15:35:52 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 const settings = {
   packname: 'STC',
   author: 'Bot',
-  botName: "STC Bot",
+  botName: "STC Bot VIP",
   botOwner: ' STC: The New Generation ', // Your name
   ownerNumber: '212617503205', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
@@ -9,7 +9,7 @@ const settings = {
   maxStoreMessages: 20, 
   storeWriteInterval: 10000,
   description: "This is a bot for managing group commands and automating tasks.",
-  version: "1.0.0",
+  version: "1.0.1",
   updateZipUrl: "https://github.com/mruniquehacker/Knightbot-MD/archive/refs/heads/main.zip",
 };
 
